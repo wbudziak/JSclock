139d5a120cba52626347fcf8eac237112fb5ff75
Wojciech Budziak <68879652+wbudziak@users.noreply.github.com>
2020-12-03 00:20:11 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const minInput = document.querySelector("#minute");
 const hourInput = document.querySelector("#hour");
 const spans = document.querySelector("span");
 const btn = document.querySelector("button");
-
+const btnRealTime = document.querySelector(".realTimeButton");
 const seconds = document.querySelector(".sekunda");
 const minutes = document.querySelector(".minuta");
 const hours = document.querySelector(".godzina");
@@ -18,8 +18,49 @@ let hourRotate = 0;
 let secValue = 0;
 let minValue = 0;
 let hourValue =0;
+let dayValue =0;
 
+function realTimeClock(){
+
+    let rtClock = new Date();
+    let rtHour = rtClock.getHours();
+    let rtMinute = rtClock.getMinutes();
+    let rtSecond = rtClock.getSeconds();
+
+    let amPm = (hours < 12)? "AM" : "PM";
+
+    rtHour = (rtHour > 12) ? rtHour - 12 : rtHour;
+    rtHour = ("0" + rtHour).slice(-2);
+    rtMinute = ("0" + rtMinute).slice(-2);
+    rtSecond = ("0" + rtSecond).slice(-2);
+
+    spans.innerHTML = `${rtHour}:${rtMinute}:${rtSecond}${amPm}`;
+    let t = setTimeout(realTimeClock,500);
+    let rotateHour =0;
+    let rotateMin = 0;
+    let rotateSec = 0;
+  
+
+    audio.play();
+    
+    for(let x = 0; x<rtSecond; x++){
+    console.log(rtSecond);
+    rotateSec += 6;
+    seconds.style.transform = `rotate(${rotateSec}deg)`;
+    }
+    for(let y = 0; y<rtMinute; y++){
+        rotateMin += 6;
+        minutes.style.transform = `rotate(${rotateMin}deg)`;
+    }
+    for(let z = 0; z<rtHour; z++){
+        rotateHour += 30;
+        hours.style.transform = `rotate(${rotateHour}deg)`;
+    }
+}
+btnRealTime.addEventListener("click",realTimeClock)
 btn.addEventListener("click",()=>{
+
+    
     audio.play();
 
     for(let i = 0; i<secInput.value ; i++){
@@ -60,6 +101,7 @@ btn.addEventListener("click",()=>{
         if(minValue >= 60){
             minValue =0;
         }
+      
         spans.textContent =  `${hourValue}:${minValue}:${secValue}`;
     }, 1000);
   
