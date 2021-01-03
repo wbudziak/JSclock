cb5e241a475d235595344a38c9dac0eb628b3ded
Wojciech Budziak <68879652+wbudziak@users.noreply.github.com>
2021-01-04 00:32:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,93 +1,147 @@
-const btn = document.querySelector(".start");
-const btnStop = document.querySelector(".stop");
-const btnReset = document.querySelector(".reset");
-const btnSave = document.querySelector(".save");
-const scores  = document.querySelector(".scores")
-let minutes = document.querySelector(".minutes");
-let sec = document.querySelector(".sec");
-let mSec =document.querySelector(".mSec");
-let timer = document.querySelector(".timer");
-let flag = 1;
+const secInput = document.querySelector("#second");
+const minInput = document.querySelector("#minute");
+const hourInput = document.querySelector("#hour");
+const digital = document.querySelector(".digital");
+const numbers = document.querySelectorAll("span");
+const btn = document.querySelector("button");
+const btnRealTime = document.querySelector(".realTimeButton");
+const seconds = document.querySelector(".sekunda");
+const minutes = document.querySelector(".minuta");
+const hours = document.querySelector(".godzina");
+const reload = document.querySelector(".reload");
+let btnSelected = true;
+let btnRealTimeSelected = true;
+const audio = new Audio('sound.mp3');
+audio.loop = true;
 
-let number = 1;
-timeMin = 0;
-timeSec = 0;
-timeMSec =0;
-let time;
-let clicked = 0;
+let secRotate = 0;
+let minRotate = 0;
+let hourRotate = 0;
+let click = 0;
 
-const span = document.createElement('span');
-const spanik = document.createElement('span');
-span.style.color = "gray";
-spanik.style.color = "gray";
-const ScoreBox = document.createElement('div');
-scores.appendChild(ScoreBox); 
-ScoreBox.appendChild(span);
-ScoreBox.appendChild(spanik);
-btn.addEventListener("click",function(){
-    
-   
-    flag+=1;
-    if(flag === 2){
-         time = setInterval(() => {
-            timeMSec++;
-            if(timeMSec === 100){
-                timeMSec = 0;
-                timeSec++;
-                if(timeSec === 60){
-                    timeSec = 0;
-                    timeMin++;
-                }
-            }
-            timer.innerText = `${("0" + timeMin).slice(-2)}:${("0" + timeSec).slice(-2)},${("0" + timeMSec).slice(-2)}`;
-            span.textContent = `Round ${number}`;
-            spanik.innerText = `${("0" + timeMin).slice(-2)}:${("0" + timeSec).slice(-2)},${("0" + timeMSec).slice(-2)}`;
-        }, 10);    
-        
-        
+let secValue = 0;
+let minValue = 0;
+let hourValue =0;
+let dayValue =0;
+
+
+
+
+function realTimeClock(){
+
+    if(realTimeClock){
+        btn.addEventListener("click",()=>{
+            location.reload();
+        })
     }
-    btn.innerText = "Start";    
-    btnStop.addEventListener("click",()=>{
-        flag =1;
-        clearInterval(time);
-        btn.innerText = "continue";
-        
-    })
+    reload.textContent = "Reload";
 
-    
-    btnReset.addEventListener("click",()=>{
-        spanik.innerHTML = `${("0" + "0").slice(-2)}:${("0" + "0").slice(-2)},${("0" + "0").slice(-2)}`;
-
-        clearInterval(time);
-        flag = 1;
-        timeMSec = 0;
-        timeSec =0;
-        timeMin = 0;
-        timer.innerText = `00:00,00`;
-        time.stop();
-    })
+  if(btnRealTimeSelected){
+    btnSelected = false;
+    let =  rtClock = new Date();
+    let =  rtHour = rtClock.getHours();
+    let =  rtMinute = rtClock.getMinutes();
+    let =  rtSecond = rtClock.getSeconds();
   
+      let amPm = (hours < 12)? "AM" : "PM";
+  
+      rtHour = (rtHour > 12) ? rtHour - 12 : rtHour;
+      rtHour = ("0" + rtHour).slice(-2);
+      rtMinute = ("0" + rtMinute).slice(-2);
+      rtSecond = ("0" + rtSecond).slice(-2);
+  
+      digital.innerHTML = `${rtHour}:${rtMinute}:<span style ="font-size:15px; color:red;">${rtSecond}</span>`;
+      let t = setTimeout(realTimeClock,500);
+      let rotateHour =0;
+      let rotateMin = 0;
+      let rotateSec = 0;
     
+  
+      audio.play();
+      
+      for(let x = 0; x<rtSecond; x++){
+      rotateSec += 6;
+      seconds.style.transform = `rotate(${rotateSec}deg)`;
+      }
+      for(let y = 0; y<rtMinute; y++){
+          rotateMin += 6;
+          minutes.style.transform = `rotate(${rotateMin}deg)`;
+      }
+      for(let z = 0; z<rtHour; z++){
+          rotateHour += 30;
+          hours.style.transform = `rotate(${rotateHour}deg)`;
+      }
+  }
+  
+}
+btnRealTime.addEventListener("click",realTimeClock)
+
+btn.addEventListener("click",()=>{
+
+    click++
+    if(click >= 2){
+        location.reload();
+    }
+    btnRealTime.addEventListener("click",()=>{
+       location.reload();
+    })
+    
+    
+    reload.textContent = "Reload";
+
+    if(btnSelected){
+        btnRealTimeSelected = false;
+        audio.play();
+   
+        for(let i = 0; i<secInput.value ; i++){
+        secValue++;
+        secRotate += 6;
+        seconds.style.transform = `rotate(${secRotate}deg)`;
+        }
+        for(let i = 0; i<minInput.value; i++){
+            minValue++
+            minRotate += 6;
+            minutes.style.transform = `rotate(${minRotate}deg)`;
+        }
+        
+        for(let i = 0;i<hourInput.value; i++){
+            hourValue++
+            hourRotate += 30;
+            hours.style.transform = `rotate(${hourRotate}deg)`;
+        }
+        
+        intervalID = setInterval(() => {
+
+            secValue++
+    
+            seconds.style.transform = `rotate(${secRotate+=6}deg)`;
+            if(secValue % 60 === 0){
+                minValue++
+                minutes.style.transform =`rotate(${minRotate+=6}deg)`;
+            }
+            if(minValue === 60){
+                minValue =0;
+                hourValue++
+                hourRotate+=30;
+                hours.style.transform = `rotate(${hourRotate}deg)`;
+            }
+            if(secValue >= 60){
+                secValue =0;
+            }
+            if(minValue >= 60){
+                minValue =0;
+            }
+            digital.innerHTML =  `${("0" + hourValue).slice(-2)}:${("0" + minValue).slice(-2)}:<span style = "font-size:15px; color:red;">${("0" + secValue).slice(-2)}</span>`;
+    
+        }, 1000);
+
+    }
+
+
 })
 
-btnSave.addEventListener("click",()=>{
-            
-    const ScoreBox = document.createElement('div');
-    const span = document.createElement('span');
-    const spanik = document.createElement('span');
-    scores.appendChild(ScoreBox);
-    ScoreBox.appendChild(span);
-    ScoreBox.appendChild(spanik);
-    span.textContent = `Round ${number++}`;
-    spanik.innerText = `${("0" + timeMin).slice(-2)}:${("0" + timeSec).slice(-2)},${("0" + timeMSec).slice(-2)}`;
-    setTimeout(() => {
-        span.classList.add("spanOn");
-        spanik.classList.add("spanOn");
-    }, 100);
-    setTimeout(() => {
-        span.classList.remove("spanOn");
-        spanik.classList.remove("spanOn");
-    }, 500);
 
 
+reload.addEventListener("click",()=>{
+ location.reload();
 })
