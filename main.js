2594c4d70625727e2b3426cc14a73492f48e3ac7
Wojciech Budziak <steamvexlex@gmail.com>
2021-02-05 03:53:47 +0100

    new code for actual time

--- main.js
+++ main.js
@@ -5,143 +5,79 @@ const digital = document.querySelector(".digital");
 const numbers = document.querySelectorAll("span");
 const btn = document.querySelector("button");
 const btnRealTime = document.querySelector(".realTimeButton");
-const seconds = document.querySelector(".sekunda");
-const minutes = document.querySelector(".minuta");
-const hours = document.querySelector(".godzina");
+const secondHand = document.querySelector(".sekunda");
+const minutesHand = document.querySelector(".minuta");
+const hourHand = document.querySelector(".godzina");
 const reload = document.querySelector(".reload");
-let btnSelected = true;
-let btnRealTimeSelected = true;
-const audio = new Audio('sound.mp3');
-audio.loop = true;
 
-let secRotate = 0;
-let minRotate = 0;
-let hourRotate = 0;
-let click = 0;
+actualTime = function(){
+    let time = new Date();
+    let actualHours = time.getHours();
+    let actualMinutes = time.getMinutes();
+    let actualSeconds= time.getSeconds();
+    let rotateHour = 0;
+    let rotateMin = 0;
+    let rotateSec = 0;
 
-let secValue = 0;
-let minValue = 0;
-let hourValue =0;
-let dayValue =0;
-
-
-
-
-function realTimeClock(){
-
-    if(realTimeClock){
-        btn.addEventListener("click",()=>{
-            location.reload();
-        })
+    for(let i =0; i<actualSeconds; i++){
+        rotateSec+=6;
+        secondHand.style.transform = `rotate(${rotateSec}deg)`;
     }
-    reload.textContent = "Reload";
+    for(let i =0; i<actualHours; i++){
+        rotateHour+=30;
+        hourHand.style.transform = `rotate(${rotateHour}deg)`;
+    }
+    for(let i =0; i<actualMinutes; i++){
+        rotateMin+=6;
+        minutesHand.style.transform = `rotate(${rotateMin}deg)`;
+    }
 
-  if(btnRealTimeSelected){
-    btnSelected = false;
-    let =  rtClock = new Date();
-    let =  rtHour = rtClock.getHours();
-    let =  rtMinute = rtClock.getMinutes();
-    let =  rtSecond = rtClock.getSeconds();
-  
-      let amPm = (hours < 12)? "AM" : "PM";
-  
-      rtHour = (rtHour > 12) ? rtHour - 12 : rtHour;
-      rtHour = ("0" + rtHour).slice(-2);
-      rtMinute = ("0" + rtMinute).slice(-2);
-      rtSecond = ("0" + rtSecond).slice(-2);
-  
-      digital.innerHTML = `${rtHour}:${rtMinute}:<span style ="font-size:15px; color:red;">${rtSecond}</span>`;
-      let t = setTimeout(realTimeClock,500);
-      let rotateHour =0;
-      let rotateMin = 0;
-      let rotateSec = 0;
-    
-  
-      audio.play();
-      
-      for(let x = 0; x<rtSecond; x++){
-      rotateSec += 6;
-      seconds.style.transform = `rotate(${rotateSec}deg)`;
-      }
-      for(let y = 0; y<rtMinute; y++){
-          rotateMin += 6;
-          minutes.style.transform = `rotate(${rotateMin}deg)`;
-      }
-      for(let z = 0; z<rtHour; z++){
-          rotateHour += 30;
-          hours.style.transform = `rotate(${rotateHour}deg)`;
-      }
-  }
-  
+// wskazówka sekundy
+
+    const secInterval = setInterval(() => {
+    rotateSec+=6;
+    secondHand.style.transform = `rotate(${rotateSec+6}deg)`;
+    // Dodałem +6, poniewaz jezeli dam animacje 1s to sekundnik sie spoznia
+    }, 1000);
+
+// wskazówka minuty
+
+   let timeLeftToFullMinute = 60000 - (actualSeconds * 1000);
+   setTimeout(() => {
+    rotateMin+=6;
+    minutesHand.style.transform = `rotate(${rotateMin}deg)`;
+    setInterval(() => {
+    rotateMin+=6;
+    minutesHand.style.transform = `rotate(${rotateMin}deg)`;
+    }, 60000);
+   }, timeLeftToFullMinute);
+
+//    wskasówka godziny
+
+   let convertToMilSec = (actualSeconds*1000) + (actualMinutes * 60000);
+   let timeLeftToFullHour = 3600000 - convertToMilSec;
+   setTimeout(() => {
+    rotateHour+=30;
+    hourHand.style.transform = `rotate(${rotateHour}deg)`;
+    setInterval(() => {
+    rotateHour+=30;
+    hourHand.style.transform = `rotate(${rotateHour}deg)`;
+    }, 3600000);
+   }, timeLeftToFullHour);
 }
-btnRealTime.addEventListener("click",realTimeClock)
+btnRealTime.addEventListener('click', actualTime)
 
-btn.addEventListener("click",()=>{
-
-    click++
-    if(click >= 2){
-        location.reload();
+digitalClock = function(){
+    digitalActualTime = function(){ 
+       setInterval(() => {
+        let time = new Date();
+        let actualHours = time.getHours();
+        let actualMinutes = time.getMinutes();
+        let actualSeconds= time.getSeconds();
+        console.log(actualHours,actualMinutes,actualSeconds);
+        digital.innerHTML = `${actualHours < 10 ? `0${actualHours}`: actualHours }:${actualMinutes < 10 ? `0${actualMinutes}`: actualMinutes }:<span style = "font-size:15px; color:red;">${actualSeconds < 10 ? `0${actualSeconds}`: actualSeconds }`;
+       }, 1000);
     }
-    btnRealTime.addEventListener("click",()=>{
-       location.reload();
-    })
-    
-    
-    reload.textContent = "Reload";
-
-    if(btnSelected){
-        btnRealTimeSelected = false;
-        audio.play();
-   
-        for(let i = 0; i<secInput.value ; i++){
-        secValue++;
-        secRotate += 6;
-        seconds.style.transform = `rotate(${secRotate}deg)`;
-        }
-        for(let i = 0; i<minInput.value; i++){
-            minValue++
-            minRotate += 6;
-            minutes.style.transform = `rotate(${minRotate}deg)`;
-        }
-        
-        for(let i = 0;i<hourInput.value; i++){
-            hourValue++
-            hourRotate += 30;
-            hours.style.transform = `rotate(${hourRotate}deg)`;
-        }
-        
-        intervalID = setInterval(() => {
-
-            secValue++
-    
-            seconds.style.transform = `rotate(${secRotate+=6}deg)`;
-            if(secValue % 60 === 0){
-                minValue++
-                minutes.style.transform =`rotate(${minRotate+=6}deg)`;
-            }
-            if(minValue === 60){
-                minValue =0;
-                hourValue++
-                hourRotate+=30;
-                hours.style.transform = `rotate(${hourRotate}deg)`;
-            }
-            if(secValue >= 60){
-                secValue =0;
-            }
-            if(minValue >= 60){
-                minValue =0;
-            }
-            digital.innerHTML =  `${("0" + hourValue).slice(-2)}:${("0" + minValue).slice(-2)}:<span style = "font-size:15px; color:red;">${("0" + secValue).slice(-2)}</span>`;
-    
-        }, 1000);
-
-    }
-
-
-})
-
-
-
-reload.addEventListener("click",()=>{
- location.reload();
-})
+    btnRealTime.addEventListener('click', digitalActualTime)
+}
+digitalClock();
