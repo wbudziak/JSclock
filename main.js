61de6331beb54499d5ca926299673c8556cd2489
Wojciech Budziak <steamvexlex@gmail.com>
2021-02-25 04:29:01 +0100

    new code - more realistic clock

--- main.js
+++ main.js
@@ -3,63 +3,29 @@ const digitalSeconds = document.querySelector(".digital-seconds");
 const secondHand = document.querySelector(".second");
 const minutesHand = document.querySelector(".minute");
 const hourHand = document.querySelector(".hour");
-actualTime = function () {
+clock = function () {
     let time = new Date();
-    let actualHours = time.getHours();
-    let actualMinutes = time.getMinutes();
-    let actualSeconds = time.getSeconds();
-    let rotateHour = 0;
-    let rotateMin = 0;
-    let rotateSec = 0;
-
-    for (let i = 0; i < actualSeconds; i++) {
-        rotateSec += 6;
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let rotateHour = (hours * 30) + (minutes/2) + (seconds/120);
+    let rotateMin = (minutes * 6) + (seconds/10);
+    let rotateSec = seconds * 6;
+    secondHand.style.transform = `rotate(${rotateSec}deg)`;
+    minutesHand.style.transform = `rotate(${rotateMin}deg)`;
+    hourHand.style.transform = `rotate(${rotateHour}deg)`;
+    setInterval(() => {
+        rotateSec +=6;
+        rotateMin += .1;
+        rotateHour += 1/120;
         secondHand.style.transform = `rotate(${rotateSec}deg)`;
-    }
-    for (let i = 0; i < actualHours; i++) {
-        rotateHour += 30;
-        hourHand.style.transform = `rotate(${rotateHour}deg)`;
-    }
-    for (let i = 0; i < actualMinutes; i++) {
-        rotateMin += 6;
         minutesHand.style.transform = `rotate(${rotateMin}deg)`;
-    }
-
-    // second hand
-
-    const secInterval = setInterval(() => {
-        rotateSec += 6;
-        secondHand.style.transform = `rotate(${rotateSec}deg)`;
-    }, 1000);
-
-    // minute hand
-
-    let timeLeftToFullMinute = 60000 - (actualSeconds * 1000);
-    setTimeout(() => {
-        rotateMin += 6;
-        minutesHand.style.transform = `rotate(${rotateMin}deg)`;
-        setInterval(() => {
-            rotateMin += 6;
-            minutesHand.style.transform = `rotate(${rotateMin}deg)`;
-        }, 60000);
-    }, timeLeftToFullMinute);
-
-    // hour hand
-
-    let convertToMilSec = (actualSeconds * 1000) + (actualMinutes * 60000);
-    let timeLeftToFullHour = 3600000 - convertToMilSec;
-    setTimeout(() => {
-        rotateHour += 30;
         hourHand.style.transform = `rotate(${rotateHour}deg)`;
-        setInterval(() => {
-            rotateHour += 30;
-            hourHand.style.transform = `rotate(${rotateHour}deg)`;
-        }, 3600000);
-    }, timeLeftToFullHour);
+        }, 1000);
 }
-actualTime()
+clock()
 
-const digitalTime = () => {
+const digitalClock = () => {
     let time = new Date();
     let actualHours = time.getHours();
     let actualMinutes = time.getMinutes();
@@ -67,5 +33,5 @@ const digitalTime = () => {
     digital.textContent = `${actualHours < 10 ? `0${actualHours}`: actualHours }:${actualMinutes < 10 ? `0${actualMinutes}`: actualMinutes }:`;
     digitalSeconds.textContent = `${actualSeconds < 10 ? `0${actualSeconds}`: actualSeconds }`;
 }
-digitalTime();
-setInterval(digitalTime,1000);
+digitalClock();
+setInterval(digitalClock,1000);
